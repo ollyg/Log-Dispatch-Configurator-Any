c184fd5cfcfb00b6b29c214da26c48107530d510
Oliver Gorwits <oliver@cpan.org>
2009-07-28 11:03:49 +0000

    Set min version of Config::Any to 0.15 so we get flatten_to_hash, use_ext

--- lib/Log/Dispatch/Configurator/Any.pm
+++ lib/Log/Dispatch/Configurator/Any.pm
@@ -7,7 +7,7 @@ use base 'Log::Dispatch::Configurator';
 use Config::Any;
 use Carp;
 
-our $VERSION = '1.0004';
+our $VERSION = '1.0005';
 $VERSION = eval $VERSION; # numify for warning-free dev releases
 # $Id$
 
@@ -82,7 +82,7 @@ Log::Dispatch::Configurator::Any - Configurator implementation with Config::Any
 
 =head1 VERSION
 
-This document refers to version 1.0004 of Log::Dispatch::Configurator::Any
+This document refers to version 1.0005 of Log::Dispatch::Configurator::Any
 
 =head1 PURPOSE
 
